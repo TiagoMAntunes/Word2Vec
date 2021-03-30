612718d36b671a35a8b72abafb9febd57751540d
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2021-03-30 08:30:52 +0100

    Avoid pre-processing due to large training size

--- word2vec/model.py
+++ word2vec/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import sys
 import numpy as np
 import time
+from util import clean_line
 
 class CBOW(torch.nn.Module):
     
@@ -22,20 +23,34 @@ class CBOW(torch.nn.Module):
 
 
 class WikiDataset(torch.utils.data.IterableDataset):
-    def __init__(self, file):        
+    def __init__(self, file, word_range):        
         self.file = file
+        self.word_range = word_range
 
     def __iter__(self):
-        return WikiData(self.file)
+        return WikiData(self.file, self.word_range)
 
 class WikiData:
-    def __init__(self, filename):
+    def __init__(self, filename, word_range):
         self.file = open(filename)
+        self.words = []
+        self.word_range = word_range
 
     def __next__(self):
-        line = tuple(map(lambda x: words_to_idx[x], next(self.file).split()))
-        mid = len(line) // 2
-        return np.array([*line[:mid], *line[mid+1:]]), np.array(line[mid])
+        """
+            Returns a word_range *2 + 1 sized np.array for training
+        """
+        while len(self.words) < self.word_range * 2 + 1:
+            line = self.file.readline()
+            if not line:
+                raise StopIteration()
+                
+            self.words.extend(list(clean_line(line)))
+        words = tuple(map(lambda x: words_to_idx[x], self.words[:self.word_range * 2 + 1]))
+        self.words = self.words[1:]
+
+        mid = len(words) // 2
+        return np.array([*words[:mid], *words[mid+1:]]), np.array(words[mid])
 
 EMBEDDING_DIM = 200
 BATCH_SIZE = 8192
@@ -43,22 +58,18 @@ NUM_WORKERS = 2
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 filename = 'tmp.txt'
+SAVE_FOLDER = "models/"
 
 with open(f'{filename}.vocab') as f:
     vocab = sorted(f.readline().split())
 VOCAB_SIZE = len(vocab)
 
-# data = []
+# String2int conversion
 words_to_idx = {i:j for j,i in enumerate(vocab)}
-        
-# data = np.array(data)
-train = WikiDataset(f'{filename}.examples.train')
-dev = WikiDataset(f'{filename}.examples.dev')
-test = WikiDataset(f'{filename}.examples.test')
 
+# Create dataset
+train = WikiDataset(f'{filename}', 2)
 train_dataloader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
-dev_dataloader = torch.utils.data.DataLoader(dev, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
-test_dataloader = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
 
 # Create model
 model = CBOW(VOCAB_SIZE, EMBEDDING_DIM)
@@ -67,63 +78,42 @@ model.to(device)
 loss_fn = nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.02, momentum=0.9)
 
-def test(dataloader):
-  model.eval()
-  avg_loss = 0
-  correct = 0
-  count = 0
-  for i, (context_t, target_t) in enumerate(dataloader):
-    context_t = context_t.to(device)
-    target_t = target_t.to(device)
-  
-    # run model
-    probs = model(context_t)
-
-    # get loss
-    loss = loss_fn(probs, target_t)
-
-    results = torch.max(probs, dim=1)[1] == target_t
-    correct += results.sum()
-    count += len(results)
-    avg_loss += loss.item()
-
-  print(f'Testing average loss is {avg_loss / i}, accuracy is {correct / count}')
-
-  def train():
-  for epoch in range(50): 
-      model.train()
-      
-      print(f'{"-"*10} Epoch {epoch} {"-"*10}')
-      start = time.time()
-      avg_loss = 0
-      correct = 0
-      count = 0
-      # batches
-      for i, (context_t, target_t) in enumerate(train_dataloader):
-      
-          optimizer.zero_grad()
-
-          context_t = context_t.to(device)
-          target_t = target_t.to(device)
+def train(train_dataloader):
+    for epoch in range(50): 
+        model.train()
         
-          # run model
-          probs = model(context_t)
+        print(f'{"-"*10} Epoch {epoch} {"-"*10}')
+        start = time.time()
+        avg_loss = 0
+        correct = 0
+        count = 0
+        # batches
+        for i, (context_t, target_t)  in enumerate(train_dataloader):
+            optimizer.zero_grad()
 
-          # get loss
-          loss = loss_fn(probs, target_t)
+            context_t = context_t.to(device)
+            target_t = target_t.to(device)
+        
+            # run model
+            probs = model(context_t)
 
-          results = torch.max(probs, dim=1)[1] == target_t
-          correct += results.sum()
-          count += len(results)
+            # get loss
+            loss = loss_fn(probs, target_t)
 
-          avg_loss += loss.item()
+            results = torch.max(probs, dim=1)[1] == target_t
+            correct += results.sum()
+            count += len(results)
 
-          # update model
-          loss.backward()
-          optimizer.step()
-      
-      end = time.time()
-      print(f'Finished epoch {epoch}, took {end - start}s, average loss is {avg_loss / i}, accuracy is {correct / count}')
-      test(dev_dataloader)
+            avg_loss += loss.item()
+            
+            # update model
+            loss.backward()
+            optimizer.step()
+        
+        end = time.time()
+        print(f'Finished epoch {epoch}, took {end - start}s, average loss is {avg_loss / i}, accuracy is {correct / count}')
+        if epoch % 5 == 0:
+            torch.save(model.state_dict(), f"{SAVE_FOLDER}cbow_{epoch}")
+        # test(dev_dataloader)
 
-train()
+train(train_dataloader)
